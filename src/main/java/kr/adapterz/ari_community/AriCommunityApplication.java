8ac73c2981669e2640c31d077df1ab0bef6529e0
Dain Kim <154444746+dvin-22@users.noreply.github.com>
2025-10-13 02:24:37 +0900

    Add files via upload

---
+++ src/main/java/kr/adapterz/ari_community/AriCommunityApplication.java
@@ -0,0 +1,13 @@
+package kr.adapterz.ari_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AriCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AriCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/adapterz/ari_community/AriCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.adapterz.ari_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AriCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
